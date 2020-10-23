6bb143041ba7266a0f7dd6098cb15f99062b5939
Jiazheng <yuanjiazheng123@gmail.com>
2020-10-22 23:43:08 -0700

    a

--- General_script.py
+++ General_script.py
@@ -6,6 +6,7 @@ def get_content(password,user):
     r = requests.post(
         "http://s32.game.baozouwushuang.com/index.php?v=0&c=login&m=user&token=&channel=9&lang=zh-cn&mac=155098868678924&devicetoken=000000&u="+user+"&p="+password+"&adid&channel=9&token=")
     content = r.content.decode()
+    print(content)
     content = json.loads(content)
     return content
 
